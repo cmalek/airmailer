e8316317b55fe699c14ee20eb67d5edc02b9b965
Chris Malek <cmalek@caltech.edu>
2024-04-18 15:55:46 -0700

    Bump version: 0.2.1 → 0.2.2

--- airmailer/__init__.py
+++ airmailer/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Christopher Malek"""
 __email__ = 'cmalek@caltech.edu'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ copyright: str = 'California Institute of Technology'
 author: str = 'Caltech IMSS ADS'
 
 # The full version, including alpha/beta/rc tags
-release: str = '0.2.1'
+release: str = '0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
